266031e4e5dd932feb5fad5bfcee1acc32025cc6
Ivan Lednev <bishop1860@gmail.com>
2021-08-20 14:03:29 +0200

    refactor: rearrange modules; fix: moving sub-items with tab chars

---
+++ src/__tests__/archiver.test.ts
@@ -0,0 +1,145 @@
+import { Archiver } from "../archiver";
+
+describe("Moving top-level tasks to the archive", () => {
+    test("No-op for files without completed tasks", () => {
+        const archiver = new Archiver();
+        const lines = ["foo", "bar", "# Archived"];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual(lines);
+    });
+
+    test("No-op for files without an archive", () => {
+        const archiver = new Archiver();
+        const lines = ["- [x] foo", "bar"];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual(lines);
+    });
+
+    test("Moves a single task to an empty archive", () => {
+        const archiver = new Archiver();
+        const lines = ["- [x] foo", "- [ ] bar", "# Archived"];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual(["- [ ] bar", "# Archived", "- [x] foo"]);
+    });
+
+    test("Moves a single task to the end of a populated archive", () => {
+        const archiver = new Archiver();
+        const lines = [
+            "- [x] foo",
+            "- [ ] bar",
+            "# Archived",
+            "- [x] Completed",
+            "- [x] Completed #2",
+        ];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual([
+            "- [ ] bar",
+            "# Archived",
+            "- [x] Completed",
+            "- [x] Completed #2",
+            "- [x] foo",
+        ]);
+    });
+
+    test("Moves multiple tasks to the end of a populated archive", () => {
+        const archiver = new Archiver();
+        const lines = [
+            "- [x] foo",
+            "- [x] foo #2",
+            "- [ ] bar",
+            "- [x] foo #3",
+            "# Archived",
+            "- [x] Completed",
+            "- [x] Completed #2",
+        ];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual([
+            "- [ ] bar",
+            "# Archived",
+            "- [x] Completed",
+            "- [x] Completed #2",
+            "- [x] foo",
+            "- [x] foo #2",
+            "- [x] foo #3",
+        ]);
+    });
+
+    test("Moves sub-items with top-level items", () => {
+        const archiver = new Archiver();
+        const lines = [
+            "- [ ] bar",
+            "- [x] foo",
+            "  stuff in the same block",
+            "    - Some info",
+            "    - [ ] A subtask",
+            "# Archived",
+            "- [x] Completed",
+        ];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual([
+            "- [ ] bar",
+            "# Archived",
+            "- [x] Completed",
+            "- [x] foo",
+            "  stuff in the same block",
+            "    - Some info",
+            "    - [ ] A subtask",
+        ]);
+    });
+
+    test("Moves sub-items with top-level items after the archive heading, indented with tabs", () => {
+        const archiver = new Archiver();
+        const lines = [
+            "- [ ] bar",
+            "# Archived",
+            "- [x] Completed",
+            "# After archive",
+            "Other stuff",
+            "- [x] foo",
+            "   stuff in the same block".replace("   ", "\t"),
+            "   - Some info".replace("   ", "\t"),
+            "   - [ ] A subtask".replace("   ", "\t"),
+        ];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual([
+            "- [ ] bar",
+            "# Archived",
+            "- [x] Completed",
+            "- [x] foo",
+            "   stuff in the same block".replace("   ", "\t"),
+            "   - Some info".replace("   ", "\t"),
+            "   - [ ] A subtask".replace("   ", "\t"),
+            "# After archive",
+            "Other stuff",
+        ]);
+    });
+
+    test("Respects newlines around headings", () => {
+        const archiver = new Archiver();
+        const lines = [
+            "- [x] foo",
+            "",
+            "",
+            "# Archived",
+            "",
+            "",
+            "- [x] Completed",
+            "",
+            "",
+            "# Another heading",
+        ];
+        const result = archiver.archiveTasks(lines);
+        expect(result).toEqual([
+            "",
+            "",
+            "# Archived",
+            "",
+            "",
+            "- [x] Completed",
+            "- [x] foo",
+            "",
+            "",
+            "# Another heading",
+        ]);
+    });
+});
---
+++ src/archiver.ts
@@ -0,0 +1,102 @@
+export class Archiver {
+    private ARCHIVE_PATTERN = new RegExp("# Archived");
+    private ARCHIVE_END_PATTERN = new RegExp("^#+\\s+(?!Archived)");
+    private COMPLETED_TASK_PATTERN = new RegExp("- \\[x\\] ");
+    private INDENTED_LINE_PATTERN = new RegExp("^( {2,}|\\t)\\s*\\S+");
+
+    constructor() {}
+
+    archiveTasks(lines: string[]) {
+        const hasArchive = lines.find((line) =>
+            this.ARCHIVE_PATTERN.exec(line)
+        );
+        if (!hasArchive) {
+            return lines;
+        }
+
+        const { fileLinesWithoutCompletedTasks, completedTasksOutsideArchive } =
+            this.extractCompletedTasks(lines);
+
+        if (completedTasksOutsideArchive.length === 0) {
+            return lines;
+        }
+
+        const linesWithInsertedArchivedTasks = this.addTasksToArchive(
+            completedTasksOutsideArchive,
+            fileLinesWithoutCompletedTasks
+        );
+        return linesWithInsertedArchivedTasks;
+    }
+
+    private extractCompletedTasks(lines: string[]) {
+        const fileLinesWithoutCompletedTasks = [];
+        const completedTasksOutsideArchive = [];
+
+        let insideArchive = false;
+        let linesAfterTask = false;
+        for (const line of lines) {
+            if (this.ARCHIVE_PATTERN.exec(line)) {
+                insideArchive = true;
+            }
+
+            if (insideArchive) {
+                if (this.ARCHIVE_END_PATTERN.exec(line)) {
+                    insideArchive = false;
+                }
+            }
+
+            if (line.match(this.COMPLETED_TASK_PATTERN) && !insideArchive) {
+                completedTasksOutsideArchive.push(line);
+                linesAfterTask = true;
+            } else if (
+                line.match(this.INDENTED_LINE_PATTERN) &&
+                linesAfterTask
+            ) {
+                completedTasksOutsideArchive.push(line);
+            } else {
+                fileLinesWithoutCompletedTasks.push(line);
+                linesAfterTask = false;
+            }
+        }
+        return { fileLinesWithoutCompletedTasks, completedTasksOutsideArchive };
+    }
+
+    private addTasksToArchive(tasks: string[], fileLines: string[]) {
+        const fileLinesWithInsertedArchivedTasks = [...fileLines];
+
+        let insideArchive = false;
+        let lastNonBlankLineIndex = null;
+        for (const [i, line] of fileLinesWithInsertedArchivedTasks.entries()) {
+            const isArchiveStart = this.ARCHIVE_PATTERN.exec(line) !== null;
+            if (isArchiveStart) {
+                insideArchive = true;
+            }
+
+            if (insideArchive) {
+                const isEndOfArchive = this.ARCHIVE_END_PATTERN.exec(line);
+                const isLastLine =
+                    i === fileLinesWithInsertedArchivedTasks.length - 1;
+                if (isEndOfArchive || isLastLine) {
+                    let insertionIndex = i + 1;
+                    if (isEndOfArchive) {
+                        if (lastNonBlankLineIndex !== null) {
+                            insertionIndex = lastNonBlankLineIndex + 1;
+                        } else {
+                            insertionIndex = i;
+                        }
+                    }
+                    fileLinesWithInsertedArchivedTasks.splice(
+                        insertionIndex,
+                        0,
+                        ...tasks
+                    );
+                    break;
+                }
+                if (line.match(/\S/)) {
+                    lastNonBlankLineIndex = i;
+                }
+            }
+        }
+        return fileLinesWithInsertedArchivedTasks;
+    }
+}
