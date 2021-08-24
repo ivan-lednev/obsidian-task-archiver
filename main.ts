996a266fa2d88e0f347ba962d1755d5cc258840b
Ivan Lednev <bishop1860@gmail.com>
2021-08-24 09:49:09 +0200

    Add a setting tab; save the weekly note pattern to the disk

--- main.ts
+++ main.ts
@@ -9,12 +9,17 @@ import {
 import { Archiver } from "src/archiver";
 
 export default class ObsidianTaskArchiver extends Plugin {
+    settings: ArchiverSettings;
     async onload() {
         this.addCommand({
             id: "archive-tasks",
             name: "Archive tasks in this file",
             callback: () => this.archiveTasksInCurrentFile(),
         });
+
+        await this.loadSettings();
+
+        this.addSettingTab(new ArchiverSettingTab(this.app, this));
     }
 
     private async archiveTasksInCurrentFile() {
@@ -30,4 +35,54 @@ export default class ObsidianTaskArchiver extends Plugin {
             linesWithInsertedArchivedTasks.join("\n")
         );
     }
+
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        );
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
+
+class ArchiverSettingTab extends PluginSettingTab {
+    plugin: ObsidianTaskArchiver;
+    constructor(app: App, plugin: ObsidianTaskArchiver) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const { containerEl } = this;
+
+        containerEl.empty();
+
+        containerEl.createEl("h2", { text: "Obsidian Task Archiver Settings" });
+
+        new Setting(containerEl)
+            .setName("Weekly note pattern")
+            .setDesc("Weekly note pattern")
+            .addMomentFormat((momentFormatComponent) => {
+                momentFormatComponent
+                    .setDefaultFormat("YYYY-MM-[W]-w")
+                    .setValue(this.plugin.settings.weeklyNoteFormat)
+                    // .setSampleEl(createEl("h2", { text: "Sample el!" }))
+                    .onChange(async (value) => {
+                        this.plugin.settings.weeklyNoteFormat = value;
+                        await this.plugin.saveSettings();
+                    });
+            });
+    }
+}
+
+interface ArchiverSettings {
+    weeklyNoteFormat: string;
+}
+
+const DEFAULT_SETTINGS: ArchiverSettings = {
+    weeklyNoteFormat: "YYYY-MM-[W]-w",
+};
