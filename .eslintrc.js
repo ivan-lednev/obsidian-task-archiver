ccef00dbf5bbddaee47e469fe1ce3783153c7521
Ivan Lednev <bishop1860@gmail.com>
2023-03-13 16:55:18 +0100

    Add eslint-plugin-lodash-fp

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
         ecmaVersion: "latest",
         sourceType: "module",
     },
-    plugins: ["@typescript-eslint", "jest", "unused-imports", "solid"],
+    plugins: ["@typescript-eslint", "jest", "unused-imports", "solid", "lodash-fp"],
     rules: {
         "jest/expect-expect": [
             "warn",
@@ -24,5 +24,36 @@ module.exports = {
                 assertFunctionNames: ["expect", "*Check*"],
             },
         ],
+        "lodash-fp/consistent-compose": "off",
+        "lodash-fp/consistent-name": ["error", "_"],
+        "lodash-fp/no-argumentless-calls": "error",
+        "lodash-fp/no-chain": "error",
+        "lodash-fp/no-extraneous-args": "error",
+        "lodash-fp/no-extraneous-function-wrapping": "error",
+        "lodash-fp/no-extraneous-iteratee-args": "error",
+        "lodash-fp/no-extraneous-partials": "error",
+        "lodash-fp/no-for-each": "off",
+        "lodash-fp/no-partial-of-curried": "error",
+        "lodash-fp/no-single-composition": "error",
+        "lodash-fp/no-submodule-destructuring": "error",
+        "lodash-fp/no-unused-result": "error",
+        "lodash-fp/prefer-compact": "error",
+        "lodash-fp/prefer-composition-grouping": "error",
+        "lodash-fp/prefer-constant": [
+            "error",
+            {
+                arrowFunctions: false,
+            },
+        ],
+        "lodash-fp/prefer-flat-map": "error",
+        "lodash-fp/prefer-get": "error",
+        "lodash-fp/prefer-identity": [
+            "error",
+            {
+                arrowFunctions: false,
+            },
+        ],
+        "lodash-fp/preferred-alias": "off",
+        "lodash-fp/use-fp": "error",
     },
 };
