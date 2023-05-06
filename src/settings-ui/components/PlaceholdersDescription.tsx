e1c8c1e27b03718b86018121cb6a3ef82f3a0d70
Ivan Lednev <bishop1860@gmail.com>
2023-05-06 10:33:35 +0200

    Add copy buttons to placeholders

--- src/settings-ui/components/PlaceholdersDescription.tsx
+++ src/settings-ui/components/PlaceholdersDescription.tsx
@@ -1,7 +1,11 @@
+import { Notice } from "obsidian";
+
 import { For, mergeProps } from "solid-js";
 
-import { DEFAULT_DATE_FORMAT, placeholders } from "../../Constants";
-import { DEFAULT_SETTINGS } from "../../Settings";
+import { Accordion } from "./Accordion";
+import { ButtonSetting } from "./setting/ButtonSetting";
+
+import { placeholders } from "../../Constants";
 import { PlaceholderService } from "../../services/PlaceholderService";
 
 interface PlaceholdersDescriptionProps {
@@ -16,42 +20,43 @@ export function PlaceholdersDescription(props: PlaceholdersDescriptionProps) {
   const sourceFilePath = () =>
     mergedProps.placeholderResolver.resolve(placeholders.ACTIVE_FILE_PATH);
 
+  const placeholdersWithDesc = () => [
+    [placeholders.DATE, "resolves to the current date in any format"],
+    [
+      placeholders.ACTIVE_FILE_NEW,
+      <>
+        for the currently open file it resolves to <b>{sourceFileName()}</b>
+      </>,
+    ],
+    [
+      placeholders.ACTIVE_FILE_PATH,
+      <>
+        for the currently open file it resolves to <b>{sourceFilePath()}</b>
+      </>,
+    ],
+    [
+      placeholders.OBSIDIAN_TASKS_COMPLETED_DATE,
+      "obsidian-tasks completed date (✅ 2023-03-20); if the task doesn't have one, defaults to today",
+    ],
+    ...mergedProps.extraPlaceholders,
+  ];
+
   return (
-    <>
-      Placeholders you can use:
-      <table>
-        <For
-          each={[
-            [placeholders.DATE, "resolves to the current date in any format"],
-            [
-              placeholders.ACTIVE_FILE_NEW,
-              <>
-                for the currently open file it resolves to <b>{sourceFileName()}</b>
-              </>,
-            ],
-            [
-              placeholders.ACTIVE_FILE_PATH,
-              <>
-                for the currently open file it resolves to <b>{sourceFilePath()}</b>
-              </>,
-            ],
-            [
-              placeholders.OBSIDIAN_TASKS_COMPLETED_DATE,
-              "obsidian-tasks completed date (✅ 2023-03-20); if the task doesn't have one, defaults to today",
-            ],
-            ...mergedProps.extraPlaceholders,
-          ]}
-        >
-          {([placeholder, description]) => (
-            <tr>
-              <td class="archiver-placeholders-description">
-                <code>{placeholder}</code>
-              </td>
-              <td>{description}</td>
-            </tr>
-          )}
-        </For>
-      </table>
-    </>
+    <Accordion title="Placeholders you can use">
+      <For each={placeholdersWithDesc()}>
+        {([placeholder, description]) => (
+          <ButtonSetting
+            name={placeholder}
+            description={description}
+            onClick={async () => {
+              await window.navigator.clipboard.writeText(placeholder as string);
+              // eslint-disable-next-line no-new
+              new Notice(`${placeholder} copied to clipboard`);
+            }}
+            buttonText="Copy to clipboard"
+          />
+        )}
+      </For>
+    </Accordion>
   );
 }
